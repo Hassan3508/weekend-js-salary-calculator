66f6950eb3bcaacf506352dda138150dce2e08a3
ismail hassan <ismailshariff350@gmail.com>
2025-01-11 08:35:03 -0600

    i grap the values from the input

--- salary.js
+++ salary.js
@@ -2,6 +2,22 @@
 
 function addEmployee(event) {
   
+  // stops the form from reloading
   event.preventDefault();
   console.log('form is ready')
+
+
+   // grab values from the input
+  const FirstName= document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const idNumber = document.getElementById("idNumber");
+  const jobTitlle = document.getElementById("jobTitle");
+  const annualSalary = document.getElementById("annualSalary");
+  
+  console.log(FirstName.value);
+  console.log(lastName.value);
+  console.log(idNumber.value);
+  console.log(jobTitlle.value);
+  console.log( annualSalary.value);
+
 }
