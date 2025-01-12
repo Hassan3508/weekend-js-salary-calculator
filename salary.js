4b03e4ff850a689892f577eec4ba25bd7679ccc4
ismail hassan <ismailshariff350@gmail.com>
2025-01-12 12:49:33 -0600

    employee object created

--- salary.js
+++ salary.js
@@ -1,22 +1,26 @@
+// array to store employee data
+const employees = [];
 
-const employess = [];
 
 function addEmployee(event) {
+    event.preventDefault(); 
 
-    // stops the form from reloading
-    event.preventDefault();
-    console.log('form is ready')
-     // grab values from the input
-    const FirstName= document.getElementById("firstName");
-    const lastName = document.getElementById("lastName");
-    const idNumber = document.getElementById("idNumber");
-    const jobTitlle = document.getElementById("jobTitle");
-    const annualSalary = document.getElementById("annualSalary");
-    
-    console.log(FirstName.value);
-    console.log(lastName.value);
-    console.log(idNumber.value);
-    console.log(jobTitlle.value);
-    console.log( annualSalary.value);
+    // Get the values from the input fields
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const idNumber = document.getElementById('idNumber').value;
+    const jobTitle = document.getElementById('jobTitle').value;
+    const annualSalary = Number(document.getElementById('annualSalary').value); // converted to number
+
+
+   // create a new employee object using shorthand method
+   const newEmployee = {
+    firstName,
+    lastName,
+    idNumber,
+    jobTitle,
+    annualSalary
+};
+
+   
 }
-
